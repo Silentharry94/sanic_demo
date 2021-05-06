593b89688183eca7ca88550860292d6aa6aa3e33
Hanley <781945719@qq.com>
2021-05-06 11:02:16 +0800

    opt app run ssl

--- app.py
+++ app.py
@@ -22,6 +22,12 @@ parser.add_argument(
 parser.add_argument(
     '-p', '--port', default=8080,
     type=int, help='start project port')
+parser.add_argument(
+    '-c', '--cert', type=str,
+    help='start project ssl cert file path')
+parser.add_argument(
+    '-k', '--key', type=str,
+    help='start project ssl key file path')
 args = parser.parse_args()
 
 
@@ -31,7 +37,13 @@ def main():
     _config = Common.yaml_config()
     app.config.update(_config)
     logging.debug(f"start {args.env} server at {args.port}")
-    app.run(host="0.0.0.0", port=args.port, backlog=2048,
+    ssl = None
+    if args.cert and args.key:
+        ssl = {
+            'cert': args.cert,
+            'key': args.key
+        }
+    app.run(host="0.0.0.0", port=args.port, backlog=2048, ssl=ssl,
             workers=os.cpu_count(), debug=False, access_log=False)
 
 
